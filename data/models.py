fd11c70e612a84439863bf087c9a0081b7552341
Begzod <begzodasidev@gmail.com>
2026-04-19 02:23:55 +0300

    fix

--- data/models.py
+++ data/models.py
@@ -6,10 +6,14 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-# DOCKER_DB_URL = os.getenv("DOCKER_DB_URL")
-# LOCAL_DB_URL = os.getenv("LOCAL_DB_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 engine = create_async_engine(DATABASE_URL, echo=False)
