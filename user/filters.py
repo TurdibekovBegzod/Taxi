fb3c5c530b83718c30aa2b909f03809e64d2bb32
Munisa <xoliyorovamunesagmail.com>
2026-04-28 05:33:04 +0500

    telefon raqam fltiri tug'rlandi

--- user/filters.py
+++ user/filters.py
@@ -2,23 +2,21 @@ import re
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-from datetime import datetime
-from .functions import check_cancel
+
 class PhoneFilter(BaseFilter):
     async def __call__(self, message: Message, state: FSMContext) -> bool:
         if not message.text and not message.contact:
-            await message.answer("📞 Telefon raqamingizni kiriting\nNamuna: +998901234567 yoki 901234567")
+            await message.answer("📞 Telefon raqamingizni kiriting\nNamuna: +998776543423 yoki 998765434")
             return False
 
         phone = message.contact.phone_number if message.contact else message.text.strip()
-        digits = re.sub(r"\D", "", phone)
 
-        if len(digits) == 9  or (len(digits) == 12 and digits.startswith("998")):
+        if re.fullmatch(r"(\+998\d{9}|\d{9})", phone):
             return True
         
-        await message.answer("❌ Telefon noto'g'ri\nNamuna: +998901234567 yoki 901234567")
+        await message.answer("❌ Telefon noto'g'ri\nNamuna: +998776543423 yoki 998765434")
         return False
-
+    
 class PeopleCountFilter(BaseFilter):
     async def __call__(self, message: Message, state: FSMContext) -> bool:
         text = (message.text or "").strip()
@@ -27,10 +25,10 @@ class PeopleCountFilter(BaseFilter):
             await message.answer("❌ Faqat raqam kiriting\n👥 Masalan: 4")
             return False
         
-        if 1 <= int(text) <= 10:
+        if 1 <= int(text) <= 20:
             return True
         
-        await message.answer("❌ Odamlar soni 1-10 oralig'ida bo'lishi kerak")
+        await message.answer("Aniq ketadigan odamlarning sonini kriting ")
         return False
     
 
